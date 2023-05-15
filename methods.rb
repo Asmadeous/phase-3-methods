c87b168d646506a7c23497104080f323ba160cf5
Aizen <iansky3311@gmail.com>
2023-05-15 16:59:06 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet (name )
+    puts "Hello, #{name}!"
+end
+greet ()
+
+def greet_with_default (name = "programmer" )
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+def add num1=1,num2=2
+    puts num1 + num2
+    return num1 + num2
+end
+add()
+
+def halve (num = 2)
+    if num.class == String
+        return nil
+    end
+
+    num / 2
+end
+halve()
